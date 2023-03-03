a52a652130810e098acf162c8cc9644d3efc8c87
Aung Kyaw Soe <33275086+ArjunKarki@users.noreply.github.com>
2023-03-03 17:19:53 +0630

    input focus on edit

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import { Todo } from "../model";
 
 import { MdDelete } from "react-icons/md";
@@ -14,17 +14,25 @@ function SingleTodo({ todos, todo, setTodos }: Props) {
   const [isEdit, setIsEdit] = useState(false);
   const [editTodo, setEditTodo] = useState(todo.todo);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleDone = (id: number) => {
-    setTodos((prev) =>
-      prev.map((todo, i) =>
-        todo.id == id ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
+    if (!isEdit) {
+      setTodos((prev) =>
+        prev.map((todo, i) =>
+          todo.id == id ? { ...todo, isDone: !todo.isDone } : todo
+        )
+      );
+    }
   };
 
   const handleEdit = (id: number) => {
     setIsEdit(true);
   };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [isEdit]);
 
   const handleDelete = (id: number) => {
     setTodos((prev) => prev.filter((todo) => todo.id != id));
@@ -45,6 +53,7 @@ function SingleTodo({ todos, todo, setTodos }: Props) {
     <form className="todo__single" onSubmit={(e) => handleSubmit(e)}>
       {isEdit ? (
         <input
+          ref={inputRef}
           value={editTodo}
           onChange={(e) => setEditTodo(e.target.value)}
           className="todos__single--text"
